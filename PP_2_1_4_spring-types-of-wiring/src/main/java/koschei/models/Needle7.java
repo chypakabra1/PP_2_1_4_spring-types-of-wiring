8a6dd01b4c03ed7953abe6a1be2afb3d57798aee
chypakabra1 <157878299+chypakabra1@users.noreply.github.com>
2024-05-22 14:37:41 +0300

    Add files via upload

---
+++ PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,13 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+    private final Deth8 deth = new Deth8();
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле " + deth.toString();
+    }
+}
